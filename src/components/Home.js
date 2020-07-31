439e2b40cd1bb19f73172897be7845316a342d78
Damhan Richardson <damhanrichardson@Gmail.com>
2020-07-31 14:19:20 -0700

    Initial commit of base createreactapp.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../App.css';
+
+
+function Home() {
+  return (
+      <div className="Home">
+      </div>
+
+  );
+}
+
+export default Home;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './../App.css';
+import {Navbar} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+
+function NavigationBar() {
+  return (
+
+      <Navbar collapseOnSelect expand="lg" className="stroke">
+        <Link to="/">
+            <Navbar.Brand className="navbar-brand hvr-grow">DaSkate</Navbar.Brand>
+        </Link>
+        <ul class="navbar-nav ml-auto">
+            <Link to='/'>
+                <li className="navbar-text navlinks">Home</li>
+            </Link>
+
+            <Link to='/'>
+                <li className="navbar-text navlinks">Shop</li>
+            </Link>
+        </ul>
+        <Link to="/">
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <ul class="navbar-nav ml-auto">
+                <Link to="/">
+                    <li class="navbar-text navlinks" >Cart</li>
+                </Link>
+                
+                <Link to="/">
+                    <li class="navbar-text navlinks" >Search</li>
+                </Link>
+                
+                <Link to="/">
+                    <li class="navbar-text navlinks" >Currency</li>
+                </Link>
+            </ul>
+        </Navbar.Collapse>
+      </Navbar>
+
+  );
+}
+
+export default NavigationBar;
