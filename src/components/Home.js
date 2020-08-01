3fb849ba184446ae4d2c3ed2ef31d562e8ca8f61
Damhan Richardson <damhanrichardson@Gmail.com>
2020-07-31 18:19:50 -0700

    Fixed navbar collapse bug's on certain screen sizes and cleaned up all compilation warnings.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './../App.css';
 import Lander from './../assets/skateboard-lander.jpg';
-import {Link} from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-
 
 function Home() {
   return (
       <div className="container-fluid">
         <div className="row">
           <div className="img-container">
-            <h1 className="bottom-left">2020 Release <br/> <a className="call-to-action">Shop Now</a> </h1>
-            <img src={Lander} className="lander-img"/>
+            <h1 className="bottom-left">2020 Release <br/> <a href="/" className="call-to-action">Shop Now</a> </h1>
+            <img alt="Skateboarding man - widescreen lander" src={Lander} className="lander-img"/>
           </div>
         </div>
       </div>
-
   );
 }
 
