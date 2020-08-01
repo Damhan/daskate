f99fd795c61144bc18584b757b7b38d6aebcc97e
damhan <damhanrichardson@gmail.com>
2020-07-31 17:10:08 -0700

    Finished navbar basics & Lander img.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './../App.css';
+import Lander from './../assets/skateboard-lander.jpg';
+import {Link} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 
 function Home() {
   return (
-      <div className="Home">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="img-container">
+            <h1 className="bottom-left">2020 Release <br/> <a className="call-to-action">Shop Now</a> </h1>
+            <img src={Lander} className="lander-img"/>
+          </div>
+        </div>
       </div>
 
   );
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,7 @@ import {Link} from 'react-router-dom';
 function NavigationBar() {
   return (
 
-      <Navbar collapseOnSelect expand="lg" className="stroke">
+      <Navbar collapseOnSelect expand="lg" className="stroke nav-main">
         <Link to="/">
             <Navbar.Brand className="navbar-brand hvr-grow">DaSkate</Navbar.Brand>
         </Link>
@@ -15,18 +15,16 @@ function NavigationBar() {
             <Link to='/'>
                 <li className="navbar-text navlinks">Home</li>
             </Link>
-
-            <Link to='/'>
-                <li className="navbar-text navlinks">Shop</li>
-            </Link>
         </ul>
-        <Link to="/">
-        </Link>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse id="responsive-navbar-nav">
         <ul class="navbar-nav ml-auto">
+                <Link to='/'>
+                    <li className="navbar-text navlinks">Shop</li>
+                </Link>
                 <Link to="/">
-                    <li class="navbar-text navlinks" >Cart</li>
+                    <li class="navbar-text navlinks" >Currency</li>
                 </Link>
                 
                 <Link to="/">
@@ -34,7 +32,7 @@ function NavigationBar() {
                 </Link>
                 
                 <Link to="/">
-                    <li class="navbar-text navlinks" >Currency</li>
+                    <li class="navbar-text navlinks" >Cart</li>
                 </Link>
             </ul>
         </Navbar.Collapse>
